8148ba1f61dfd1ec055c38c02174bb0100423b1f
edwin samir <patino8308@gmail.com>
2020-05-08 19:55:35 +0200

    Updates

---
+++ precache-manifest.dd57445c345e10dbdefaee93caaf85eb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd21f33adf9be2da6c886d7391a9acc0",
+    "url": "/twittor-espg/index.html"
+  },
+  {
+    "revision": "ba38feb17ede1d25a408",
+    "url": "/twittor-espg/static/css/2.1d9b8576.chunk.css"
+  },
+  {
+    "revision": "8409619bbaa0d591729f",
+    "url": "/twittor-espg/static/css/main.8e1c6a52.chunk.css"
+  },
+  {
+    "revision": "ba38feb17ede1d25a408",
+    "url": "/twittor-espg/static/js/2.6863deb8.chunk.js"
+  },
+  {
+    "revision": "8bb92c23487eb5c952e51992947cb95e",
+    "url": "/twittor-espg/static/js/2.6863deb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8409619bbaa0d591729f",
+    "url": "/twittor-espg/static/js/main.c7a36239.chunk.js"
+  },
+  {
+    "revision": "3da687f2387c33b0a35a",
+    "url": "/twittor-espg/static/js/runtime-main.8fe67099.js"
+  },
+  {
+    "revision": "16e35662baa4ca9e4f4344fd51fb7ec6",
+    "url": "/twittor-espg/static/media/camara.16e35662.svg"
+  },
+  {
+    "revision": "cad7008135a58fda8ec20be42808a6b2",
+    "url": "/twittor-espg/static/media/close.cad70081.svg"
+  },
+  {
+    "revision": "9b45fe492d3b935b1a283ece2e1b5ac8",
+    "url": "/twittor-espg/static/media/date.9b45fe49.svg"
+  },
+  {
+    "revision": "d3c57e3b34eea06328b31dbb81bcabc4",
+    "url": "/twittor-espg/static/media/error-404.d3c57e3b.png"
+  },
+  {
+    "revision": "a108520eea84cd603eeaf8eeda7676b1",
+    "url": "/twittor-espg/static/media/link.a108520e.svg"
+  },
+  {
+    "revision": "02f2070b4161235fe7d8909276457b53",
+    "url": "/twittor-espg/static/media/location.02f2070b.svg"
+  },
+  {
+    "revision": "41f65f75cb0b41db6fe44ab4b074bacc",
+    "url": "/twittor-espg/static/media/logo-white.41f65f75.png"
+  },
+  {
+    "revision": "1769587d7e1330e9d4241c30f672c7ff",
+    "url": "/twittor-espg/static/media/logo.1769587d.png"
+  }
+]);
